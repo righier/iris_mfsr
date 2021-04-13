3f0f0eae4dbc0c2124bc7545a992de373bf16601
Ermanno Righini <ermannorighini@gmail.com>
2021-04-13 20:31:35 +0200

    models refactoring

--- models.py
+++ models.py
@@ -13,13 +13,11 @@ def wn_conv3d(inc, outc, ksize, stride=1, padding=0, weight_norm=True, padding_m
   module = nn.Conv3d(inc, outc, ksize, stride, padding, padding_mode=padding_mode)
   return nn.utils.weight_norm(module) if weight_norm else module
 
-class wdsr_block(nn.Module):
-
-  def __init__(self, n_filters, expansion, weight_norm=True, low_rank_ratio = 0.8):
+class wdsr3d_block(nn.Module):
+  def __init__(self, n_filters, expansion=6, weight_norm=True, low_rank_ratio = 0.8):
     super(wdsr_block, self).__init__()
-
     self.conv = nn.Sequential(
-      wn_conv3d(n_filters, n_filters * expansion, 1, weight_norm=weight_norm),
+      wn_conv3d(n_filters, n_filters * expansion, 1, weight_norm=weight_norm), 
       nn.ReLU(inplace=True),
       wn_conv3d(n_filters * expansion, int(n_filters * low_rank_ratio), 1, weight_norm=weight_norm),
       wn_conv3d(int(n_filters * low_rank_ratio), n_filters, 3, padding=1, weight_norm=weight_norm)
@@ -28,11 +26,9 @@ class wdsr_block(nn.Module):
   def forward(self, x):
     return x + self.conv(x)
 
+class Model3DCommon(nn.Module):
 
-
-class Model3DWDSRnet(nn.Module):
-
-  def __init__(self, scale=2, frames=7, n_layers=3, n_filters=32, expansion=1, weight_norm=True, ksize=3):
+  def __init__(self, res_block, upsample, scale=2, frames=7, n_layers=8, n_filters=32, expansion=6, weight_norm=True, ksize=3):
     super(Model3DWDSRnet, self).__init__()
     self.scale = scale
 
@@ -40,68 +36,31 @@ class Model3DWDSRnet(nn.Module):
     bod2_cnt = (frames // (ksize - 1)) - 1
 
     self.convPass = nn.Sequential(
-        wn_conv3d(1, n_filters, ksize, 1, 1),
-        relu,
-        *repeat(wdsr_block, (n_filters, expansion), n_layers),
-        *repeat(nn.Conv3d, (n_filters, n_filters, ksize, 1, (0,1,1)), bod2_cnt, relu),
-        nn.Conv3d(n_filters, scale*scale, ksize, 1, (0,1,1)),
+      wn_conv3d(1, n_filters, ksize, 1, 1),
+      relu,
+      *repeat(res_block, (n_filters, expansion, weight_norm), n_layers),
+      *repeat(wn_conv3d, (n_filters, n_filters, ksize, 1, (0,1,1), weight_norm, 'reflect'), bod2_cnt, relu),
+      wn_conv3d(n_filters, scale*scale, ksize, 1, (0,1,1), weight_norm),
     )
 
-    self.upsample = nn.Upsample(scale_factor=scale, mode='bicubic', align_corners=False)
+    self.upsample = upsample
   
   def forward(self, x):
     lr = x[:, :, 3] #picks the image in the middle of the video sequence @TODO: try to pass the middle image as a separate input
-
-    x = self.convPass(x)
-    #up = self.bicubic(lr)
-
-    #reshapes the tensor, so that the scale*scale channels become sub-pixels of the high resolution image
-    x = x.squeeze(dim=2) # pixel_shuffle expects 4D tensors so we remove the depth dimension that is == 1
-    x = F.pixel_shuffle(x, self.scale) # reorders pixels
-
     y = self.upsample(lr)
-    # adds together the residual path with the upscaled image
-    x = x.add(up)
-    
-    return x
-
-class Model3DSRnet(nn.Module):
-
-  def __init__(self, scale=2, frames=7, n_layers=3, n_chan=64, ksize=3):
-    super(Model3DSRnet, self).__init__()
-    self.scale = scale
-    relu = nn.ReLU(inplace=True)
-
-    head = nn.Conv3d(1, n_chan, ksize, 1, 1)
-    bod1 = repeat(nn.Conv3d, (n_chan, n_chan, ksize, 1, 1), n_layers, activation)
-
-    # at every convolution the depth shrinks by k_size - 1
-    bod2_cnt = (frames // (ksize - 1)) - 1
-    bod2 = repeat(nn.Conv3d, (n_chan, n_chan, ksize, 1, (0,1,1)), bod2_cnt, activation)
-    tail = nn.Conv3d(n_chan, scale*scale, ksize, 1, (0,1,1))
-
-    self.convPass = nn.Sequential(
-      nn.Conv3d(1, n_chan, ksize, 1, 1), relu,
-      repeat(nn.Conv3d, (n_chan, n_chan, ksize, 1, 1), n_layers, activation)
-    )
-
-    self.convPass = nn.Sequential(head, activation, *bod1, *bod2, tail)
-    #self.bicubic = nn.Upsample(scale_factor=scale, mode='bicubic', align_corners=False)
-  
-  def forward(self, x):
-    lr = x[:, :, 3] #picks the image in the middle of the video sequence @TODO: try to pass the middle image as a separate input
-    up = F.interpolate(lr, scale_factor=self.scale, mode='bicubic', align_corners=False)
 
     x = self.convPass(x)
-
-    #up = self.bicubic(lr)
-
-    #reshapes the tensor, so that the scale*scale channels become sub-pixels of the high resolution image
     x = x.squeeze(dim=2) # pixel_shuffle expects 4D tensors so we remove the depth dimension that is == 1
     x = F.pixel_shuffle(x, self.scale) # reorders pixels
 
-    # adds together the residual path with the upscaled image
-    x = x.add(up)
-    
+    x = x.add(y)
     return x
 
+def Model3DWDSRnet(res_block=wdsr3d_block, upsample='bicubic', scale=2, frames=7, n_layers=8, n_filters=32, expansion=6, weight_norm=True, ksize=3):
+  if upsample == 'bicubic':
+    upsample = nn.Upsample(scale_factor=scale, mode='bicubic', align_corners=False)
+  return Model3DCommon(res_block, upsample, scale, frames, n_layers, n_filters, expansion, weight_norm, ksize)
+
+def Model3DSRnet(scale=2, frames=7, n_layers=4, n_filters=64, weight_norm=False, ksize=3):
+  upsample = nn.Upsample(scale_factor=scale, mode='bicubic', align_corners=False)
+  return Model3DCommon(res_block, upsample, scale, frames, n_layers, n_filters, expansion, weight_norm, ksize)
