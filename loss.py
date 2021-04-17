e73f6690a9b5496b96639ab87c29327a9dc9221d
Ermanno <ermannorighini@gmail.com>
2021-04-17 08:25:35 +0200

    added evaluation during epochs

--- loss.py
+++ loss.py
@@ -2,40 +2,48 @@ import torch
 import torch.nn as nn
 import torchvision
 
+
 def psnr(x, y):
   EPS = 1e-8
-  mse = torch.mean((x - y) ** 2, dim=(1,2,3))
+  mse = torch.mean((x - y) ** 2, dim=(1, 2, 3))
   scores = -10 * torch.log10(mse + EPS)
   return torch.mean(scores, dim=0)
 
 class VggLoss(nn.Module):
-    def __init__(self, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], grayscale=True, add_l1=True):
-        super(VggLoss, self).__init__()
-        
-        self.add_l1=add_l1
-        self.grayscale = grayscale
-        self.register_buffer('mean', torch.Tensor(mean).view(1,3,1,1))
-        self.register_buffer('std', torch.Tensor(std).view(1,3,1,1))
-        
-        cuts = [0, 4, 9, 16, 23]
-        vgg_feat = torchvision.models.vgg16(pretrained=True).features
-        self.blocks = nn.ModuleList(vgg_feat[cuts[i]: cuts[i+1]].eval() for i in range(4))
-        for param in self.parameters(): param.requires_grad = False
-    
-    def preprocess(self, x):
-        if self.grayscale: x = x.repeat(1, 3, 1, 1)
-        x = (x - self.mean) / self.std
-        return x
-        
-    def forward(self, pred, label):
-        loss = 0.0
-        if self.add_l1: loss += nn.functional.l1_loss(pred, label)
+  
+  def __init__(self, mean=None, std=None, grayscale=True, add_l1=True):
+    super(VggLoss, self).__init__()
+ 
+    if not mean: mean = [0.485, 0.456, 0.406]
+    if not std: std = [0.229, 0.224, 0.225]
 
-        pred = self.preprocess(pred)
-        label = self.preprocess(label)
+    self.add_l1 = add_l1
+    self.grayscale = grayscale
+    self.register_buffer('mean', torch.Tensor(mean).view(1, 3, 1, 1))
+    self.register_buffer('std', torch.Tensor(std).view(1, 3, 1, 1))
 
-        for bl in self.blocks:
-            pred = bl(pred)
-            label = bl(label)
-            loss += nn.functional.l1_loss(pred, label)
-        return loss
+    cuts = [0, 4, 9, 16, 23]
+    vgg_feat = torchvision.models.vgg16(pretrained=True).features
+    self.blocks = nn.ModuleList(vgg_feat[cuts[i]: cuts[i+1]].eval() for i in range(4))
+    for param in self.parameters():
+      param.requires_grad = False
+
+  def preprocess(self, x):
+    if self.grayscale: 
+      x = x.repeat(1, 3, 1, 1)
+    x = (x - self.mean) / self.std
+    return x
+
+  def forward(self, pred, label):
+    loss = 0.0
+    if self.add_l1:
+      loss += nn.functional.l1_loss(pred, label)
+
+    pred = self.preprocess(pred)
+    label = self.preprocess(label)
+
+    for bl in self.blocks:
+      pred = bl(pred)
+      label = bl(label)
+      loss += nn.functional.l1_loss(pred, label)
+    return loss
--- trainer.py
+++ trainer.py
@@ -36,7 +36,7 @@ def train_batch(device, model, X, Y, criterion, optimizer):
   loss = float(loss)
   return loss
 
-def train(cfg, device, model, train_loader, test_loader, criterion, accuracy_func, optimizer, scheduler=None, freq_scheduler_update=False, log_freq=50):
+def train(cfg, device, model, train_loader, test_loader, criterion, accuracy_func, optimizer, scheduler=None, freq_scheduler_update=False, log_freq=50, eval_freq=200):
 
   train_loss = 0
   batch_count = 0
@@ -45,10 +45,12 @@ def train(cfg, device, model, train_loader, test_loader, criterion, accuracy_fun
   wandb.watch(model, criterion, log='all', log_freq=log_freq)
   
   for epoch in tqdm(range(cfg.epochs), desc="Epochs"):
-
-    model.train()
-
     for X, Y in tqdm(train_loader, desc="Training", leave=False):
+      if batch_count % eval_freq == 0:
+        test_loss, accuracy = test(device, model, test_loader, criterion, accuracy_func)
+        wandb.log({"epoch": epoch, "test_loss": test_loss, "accuracy": accuracy, "samples"}, step=elem_count)
+        model.train()
+
       loss_batch = train_batch(device, model, X, Y, criterion, optimizer)
 
       if scheduler and freq_scheduler_update:
@@ -58,15 +60,15 @@ def train(cfg, device, model, train_loader, test_loader, criterion, accuracy_fun
       batch_count += 1
       elem_count += cfg.batch_size
 
-      if batch_count == log_freq:
-        train_loss /= batch_count
+      if batch_count % log_freq == 0:
+        train_loss /= log_freq
         learning_rate = optimizer.param_groups[0]['lr']
         wandb.log({"train_loss": train_loss, "learning_rate": learning_rate}, step=elem_count)
         batch_count = 0
         train_loss = 0
+      
     
-    test_loss, accuracy = test(device, model, test_loader, criterion, accuracy_func)
-    wandb.log({"epoch": epoch, "test_loss": test_loss, "accuracy": accuracy}, step=elem_count)
+    
 
     if scheduler and not freq_scheduler_update:
       scheduler.step(test_loss)
